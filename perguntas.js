89e734dbdf25305b468f18102e8de0ed5e468acc
Ladreu <valtinowellyngton@gmail.com>
2024-11-22 11:31:24 -0300

    Update perguntas.js

--- perguntas.js
+++ perguntas.js
@@ -1,13 +1,13 @@
 criaCartao(
     'filosofia',
     'De quem é a famosa frase “Penso, logo existo”?',
-    'Descartes.'
+    'Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650). Ela resume o pensamento e o método de Descartes, para quem tudo tem início na dúvida.'
 )
 
 criaCartao(
     'Geografia',
     'Quais o menor e o maior país do mundo?',
-    'Vaticano e Rússia.'
+    'O Vaticano, sede da igreja católica, tem apenas 44 hectares (0,44 km2). A Rússia, localizada em dois continentes (Ásia e Europa), tem 17 milhões de km2.'
 )
 
 criaCartao(
